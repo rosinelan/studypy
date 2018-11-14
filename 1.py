1cb292d556ff6827f48cf0c21ba0ed1928f774f6
rosinelan <mqwanghui327@hotmail.com>
2018-11-14 18:46:49 +0800

    wtote a python file

---
+++ 1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+num1 = input("Please a number:")
+num2 = input("Please a number:")
+
+print "%s + %s = %s" %(num1,num2,num1+num2)
+print "%s - %s = %s" %(num1,num2,num1-num2)
+print "%s * %s = %s" %(num1,num2,num1*num2)
+print "%s / %s = %s" %(num1,num2,num1/num2)
+
